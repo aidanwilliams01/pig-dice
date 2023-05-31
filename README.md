178e284cc1cfb96317f7f1004e427b5cc7b095b3
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-31 14:18:52 -0700

    add diceRoll function

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Describe: Scores()
 Test: It should return an object with three properties.
 Code: new Score('test', 'test', 'test')
 Expected Output: Scores {playerId: 'test', totalScore: 'test', currentScore: 'test'}
+
+Describe: diceRoll()
+
+Test: It should return an integer between 1 and 6.
+Code: diceRoll()
+Expected Output: an integer between 1 and 6
--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ function Scores(playerId, totalScore, currentScore) {
   this.totalScore = totalScore;
   this.currentScore = currentScore;
 }
+
+function diceRoll() {
+  min = Math.ceil(1);
+  max = Math.floor(7);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+// Scores.prototype.diceRoll = function() {
+  
+// }
