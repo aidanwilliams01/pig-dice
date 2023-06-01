7ec883330051ab52ef94f8f1a285b34a9b3a929d
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-01 16:15:23 -0700

    update HTML and Javascript to display score totals, player turn, and a message when a player has met the win condition

--- js/scripts.js
+++ js/scripts.js
@@ -50,12 +50,30 @@ function onPageLoad() {
     const submissionType = document.activeElement.value;
     if (submissionType === 'roll') {
       playerTurn = roll(playerTurn, score1, score2);
+      document.querySelector("#turnScore1").innerText = score1.turnScore;
+      document.querySelector("#turnScore2").innerText = score2.turnScore;
     }
     else {
       playerTurn = hold(playerTurn, score1, score2);
+      document.querySelector("#turnScore1").innerText = 0;
+      document.querySelector("#turnScore2").innerText = 0;
     }
-    // document.querySelector("p").removeAttribute("class");
-    // document.querySelector("p").innerText = outputString;
+    if (playerTurn % 2 != 0) {
+      document.querySelector("#playerTurn").innerText = 1;
+    }
+    else {
+      document.querySelector("#playerTurn").innerText = 2;
+    }
+    if (score1.totalScore + score1.turnScore >= 100) {
+      document.querySelector("p").innerText = `Player 1 wins`;
+      form.removeEventListener("submit", formSubmission);
+    }
+    else if (score2.totalScore + score2.turnScore >= 100) {
+      document.querySelector("p").innerText = `Player 2 wins`;
+      form.removeEventListener("submit", formSubmission);
+    }
+    document.querySelector("#totalScore1").innerText = score1.totalScore;
+    document.querySelector("#totalScore2").innerText = score2.totalScore;
   }
   form.addEventListener("submit", formSubmission);
 }
