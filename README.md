19f19942ced76a0bec5a9f6dbcffeef2f36ab05d
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-01 13:55:43 -0700

    add roll function; add initial UI logic and HTML

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Describe: diceRoll()
 Test: It should return an integer between 1 and 6.
 Code: diceRoll()
 Expected Output: an integer between 1 and 6
+
+Describe: roll(playerTurn)
+
+Test: It should modify the relevant object or increment the playerTurn variable depending on the value returned by diceRoll.
+Code:
+let score1 = new Score(0, 0)
+roll(1)
+score1
+Expected Output: Score {totalScore: 0, turnScore: 3}
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-function Scores(playerId, totalScore, currentScore) {
-  this.playerId = playerId;
+function Score(totalScore, turnScore) {
   this.totalScore = totalScore;
-  this.currentScore = currentScore;
+  this.turnScore = turnScore;
 }
 
 function diceRoll() {
@@ -10,6 +9,40 @@ function diceRoll() {
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
-// Scores.prototype.diceRoll = function() {
-  
-// }
+function roll(playerTurn) {
+  let rollAmount = diceRoll();
+  if (rollAmount === 1) {
+    score1.turnScore = 0;
+    return playerTurn += 1;
+  }
+  else {
+    if (playerTurn % 2 != 0) {
+      score1.turnScore = score1.turnScore + rollAmount;
+    }
+    else {
+      score2.turnScore = score2.turnScore + rollAmount;
+    }
+  }
+}
+
+function onPageLoad() {
+  const form = document.querySelector("form");
+  let score1 = new Score(0, 0);
+  let score2 = new Score(0, 0);
+  let playerTurn = 1;
+  function formSubmission(event) {
+    event.preventDefault();
+    const submissionType = document.activeElement.value;
+    if (submissionType === 'roll') {
+      roll(playerTurn);
+    }
+    else {
+      
+    }
+    document.querySelector("p").removeAttribute("class");
+    document.querySelector("p").innerText = outputString;
+  }
+  form.addEventListener("submit", formSubmission);
+}
+
+window.addEventListener("load", onPageLoad);
