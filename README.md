58b74c54776bc0846246802afa6cef3a1ff64dd4
Aidan Williams <aidanwilliams01@gmail.com>
2023-06-01 14:59:56 -0700

    add arguments to roll and hold functions; modify functions to return incremented playerTurn variable; update README and HTML

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-Describe: Scores()
+Describe: Score(totalScore, turnScore)
 
-Test: It should return an object with three properties.
-Code: new Score('test', 'test', 'test')
-Expected Output: Scores {playerId: 'test', totalScore: 'test', currentScore: 'test'}
+Test: It should return an object with two properties.
+Code: new Score('test', 'test')
+Expected Output: Scores {totalScore: 'test', currentScore: 'test'}
 
 Describe: diceRoll()
 
@@ -10,7 +10,7 @@ Test: It should return an integer between 1 and 6.
 Code: diceRoll()
 Expected Output: an integer between 1 and 6
 
-Describe: roll(playerTurn)
+Describe: roll(playerTurn, score1, score2)
 
 Test: It should modify the relevant object or increment the playerTurn variable depending on the value returned by diceRoll.
 Code:
@@ -19,7 +19,7 @@ roll(1)
 score1
 Expected Output: Score {totalScore: 0, turnScore: 3}
 
-Describe: hold(playerTurn)
+Describe: hold(playerTurn, score1, score2)
 
 Test: It should modify the relevant object by adding the turnScore to the totalScore.
 Code:
@@ -27,4 +27,4 @@ let score1 = new Score(0, 0)
 roll(1)
 hold(1)
 score1
-Expected Output: Score {totalScore: 3, turnScore: 3}
+Expected Output: Score {totalScore: 3, turnScore: 0}
--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,12 @@ function diceRoll() {
   return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
-function roll(playerTurn) {
+function roll(playerTurn, score1, score2) {
   let rollAmount = diceRoll();
   if (rollAmount === 1) {
     score1.turnScore = 0;
-    return playerTurn += 1;
+    score2.turnScore = 0;
+    playerTurn += 1;
   }
   else {
     if (playerTurn % 2 != 0) {
@@ -23,17 +24,20 @@ function roll(playerTurn) {
       score2.turnScore = score2.turnScore + rollAmount;
     }
   }
+  return playerTurn;
 }
 
-function hold(playerTurn) {
-  if (playerTurn === 1) {
+function hold(playerTurn, score1, score2) {
+  if (playerTurn % 2 != 0) {
     score1.totalScore += score1.turnScore;
     score1.turnScore = 0;
   }
   else {
     score2.totalScore += score2.turnScore;
-    score1.turnScore = 0;
+    score2.turnScore = 0;
   }
+  playerTurn += 1;
+  return playerTurn;
 }
 
 function onPageLoad() {
@@ -45,10 +49,10 @@ function onPageLoad() {
     event.preventDefault();
     const submissionType = document.activeElement.value;
     if (submissionType === 'roll') {
-      roll(playerTurn);
+      playerTurn = roll(playerTurn, score1, score2);
     }
     else {
-      hold(playerTurn);
+      playerTurn = hold(playerTurn, score1, score2);
     }
     // document.querySelector("p").removeAttribute("class");
     // document.querySelector("p").innerText = outputString;
